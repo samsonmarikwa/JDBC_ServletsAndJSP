ab6d4b694384425264b3a2df7aa8139595a3120b
Samson Marikwa <samsonmarikwa@outlook.com>
2022-09-05 16:36:53 -0400

    Configure the url to the HelloWorldServlet in web.xml

--- ServletBasics/src/main/java/com/samsonmarikwa/training/servlets/HelloWorldServlet.java
+++ ServletBasics/src/main/java/com/samsonmarikwa/training/servlets/HelloWorldServlet.java
@@ -8,6 +8,18 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 
+/*
+ * @WebServlet(urlPatterns = "/hello")
+ * The above annotation can be configured in place of the following entry in the web.xml file.
+ * 	<servlet>
+		<servlet-name>HelloServlet</servlet-name>
+		<servlet-class>com.samsonmarikwa.training.servlets.HelloWorldServlet</servlet-class>
+	</servlet>
+	<servlet-mapping>
+		<servlet-name>HelloServlet</servlet-name>
+		<url-pattern>/hello</url-pattern>
+	</servlet-mapping>
+ */
 public class HelloWorldServlet extends GenericServlet {
 
 	@Override
