a62b801f38f9988d5b7b94f67937937d4dd1a818
Samson Marikwa <samsonmarikwa@outlook.com>
2022-09-05 16:16:47 -0400

    Create HelloWorldServlet

---
+++ ServletBasics/src/main/java/com/samsonmarikwa/training/servlets/HelloWorldServlet.java
@@ -0,0 +1,25 @@
+package com.samsonmarikwa.training.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+public class HelloWorldServlet extends GenericServlet {
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>Hello Servlets World");
+		out.println("</body>");
+		out.println("</html>");
+
+	}
+
+}
