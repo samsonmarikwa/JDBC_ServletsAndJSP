5b12b9eb5523a83004f95f61886fc17301ee4cb7
Samson Marikwa <samsonmarikwa@outlook.com>
2022-09-06 23:23:49 -0400

    Create project and CreateUserServlet

---
+++ UserApp/src/main/java/com/samsonmarikwa/user/servlets/CreateUserServlet.java
@@ -0,0 +1,31 @@
+package com.samsonmarikwa.user.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/addServlet")
+public class CreateUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	public void init() {
+		
+	}
+       
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	public void destroy() {
+		
+	}
+
+}
