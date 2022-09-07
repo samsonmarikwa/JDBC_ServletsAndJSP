65ffc807df63482002a6f92660adbb5656983d83
Samson Marikwa <samsonmarikwa@outlook.com>
2022-09-07 00:05:38 -0400

    Implement the doPost method

--- UserApp/src/main/java/com/samsonmarikwa/user/servlets/CreateUserServlet.java
+++ UserApp/src/main/java/com/samsonmarikwa/user/servlets/CreateUserServlet.java
@@ -6,39 +6,65 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 @WebServlet("/addServlet")
 public class CreateUserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private Connection connection;
-	
+
 	public void init() {
 		try {
+			// Before Java6 Service Provider Mechanism we had to load the driver class. This
+			// is required for Tomcat as
+			// this the auto-loading of the class is disabled due to memory leaks and we are
+			// required to load the class manually.
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			connection = DriverManager.getConnection("jdbc:mysql://localhost/mydb", "root", "P@ssW0rd");
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+
+		try {
+			Statement statement = connection.createStatement();
+			int result = statement.executeUpdate(
+					"insert into user values('" + firstName + "','" + lastName + "','"+ email + "','" + password + "')");
+			PrintWriter out = response.getWriter();
+
+			if (result > 0) {
+				out.print("<H1>USER CREATED</H1>");
+			} else {
+				out.print("<H1>Error Creating the User</H1>");
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-       
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-	
 	public void destroy() {
 		try {
 			connection.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
