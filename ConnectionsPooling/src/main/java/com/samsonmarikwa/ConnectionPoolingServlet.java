959738c9ea55c76d8ec979bd92ae0d7fdb3acd91
Samson Marikwa <samsonmarikwa@outlook.com>
2022-09-15 17:28:38 -0400

    Connection pooling

---
+++ ConnectionsPooling/src/main/java/com/samsonmarikwa/ConnectionPoolingServlet.java
@@ -0,0 +1,37 @@
+package com.samsonmarikwa;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+/**
+ * Servlet implementation class ConnectionPoolingServlet
+ */
+public class ConnectionPoolingServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			Context context = new InitialContext();
+			DataSource ds = (DataSource) context.lookup("java:comp/env/myds"); // myds is coming from the context.xml file
+			Connection connection = ds.getConnection();
+			System.out.println(connection);
+		} catch (NamingException | SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
